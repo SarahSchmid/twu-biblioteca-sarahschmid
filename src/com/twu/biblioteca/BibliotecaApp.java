45fb511de4d2446e81ed29ca3dc04f4ac102d365
SarahSchmid <sarah.schmid@thoughtworks.com>
2019-01-24 12:15:05 +0100

    ssc / 1.1 / show welcome message on startup

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,8 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
+        Welcome welcome = new Welcome(System.out);
+        welcome.showWelcomeMessage();
     }
 }
