7a4e99cd75018ca1e926a0dee03820026a3743f7
SarahSchmid <sarah.schmid@thoughtworks.com>
2019-01-24 15:26:02 +0100

    ssc / 1.2 / print booklist

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,10 +1,22 @@
 package com.twu.biblioteca;
 
+import java.util.Arrays;
+
 public class BibliotecaApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        Book[] bookList = {
+                new Book("Lord ot the Rings: Fellowship of the Ring"),
+                new Book("Lord ot the Rings: The Two Towers"),
+                new Book("Lord ot the Rings: The Return of the King"),
+                new Book("Bible")
+        };
+        Library library = new Library(Arrays.asList(bookList));
 
         Welcome welcome = new Welcome(System.out);
         welcome.showWelcomeMessage();
+        System.out.println(library.showBookList());
+
     }
 }
