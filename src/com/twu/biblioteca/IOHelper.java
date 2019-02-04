c594962d1b98bc324117052104e06daac99f13c2
SarahSchmid <sarah.schmid@thoughtworks.com>
2019-02-04 19:15:46 +0100

    ssc / 2.3 / added scanner method to read a line input

--- src/com/twu/biblioteca/IOHelper.java
+++ src/com/twu/biblioteca/IOHelper.java
@@ -5,9 +5,13 @@ import java.util.Scanner;
 
 public class IOHelper {
 
-    public static int readUserInput(InputStream inputStream) {
+    public static int readSingleNumberInput(InputStream inputStream) {
         Scanner scanner = new Scanner(inputStream);
         return scanner.nextInt();
     }
 
+    public static String readLineInput(InputStream inputStream) {
+        Scanner scanner = new Scanner(inputStream);
+        return scanner.nextLine();
+    }
 }
--- test/com/twu/biblioteca/IOHelperTest.java
+++ test/com/twu/biblioteca/IOHelperTest.java
@@ -10,14 +10,24 @@ import static org.junit.Assert.assertEquals;
 public class IOHelperTest {
 
     @Test
-    public void readUserInput_should_read_the_user_input() {
+    public void readSingleNumberInput_should_read_the_user_input() {
         String input = "1";
         InputStream inputStream = new ByteArrayInputStream(input.getBytes());
         System.setIn(inputStream);
 
-        int result = IOHelper.readUserInput(inputStream);
+        int result = IOHelper.readSingleNumberInput(inputStream);
 
         assertEquals(1, result);
     }
 
+    @Test
+    public void readLineInput_should_return_the_user_input() {
+        String input = "this is an input";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+
+        String result = IOHelper.readLineInput(inputStream);
+
+        assertEquals(input, result);
+    }
 }
